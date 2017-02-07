96bc34cf249af6b0b8e3e0875e2eaddde7d66fb2
samrafaraz <samra.feraz@gmail.com>
2017-02-07 13:12:38 +1100

    My solution File going to github

---
+++ SamraProfile/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SamraProfile.Startup))]
+namespace SamraProfile
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
